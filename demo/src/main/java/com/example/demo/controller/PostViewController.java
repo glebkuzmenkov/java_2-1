80e2a44a65cb09ae686cc455fa38a31567c52dc3
glebkuzmenkov <glebkuzmenkov0827@gmail.com>
2026-02-12 11:30:20 +0300

    first commit

---
+++ demo/src/main/java/com/example/demo/controller/PostViewController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class PostViewController {
+    @ResponseBody
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    public String list()
+    {
+        return "Здесь будет главная страница.";
+    }
+
+    @ResponseBody
+    @RequestMapping(path = "/post/{id}", method = RequestMethod.GET)
+    public String single(@PathVariable("id") Long id)
+    {
+        return "Здесь будет страница поста №" + id;
+    }
+}
